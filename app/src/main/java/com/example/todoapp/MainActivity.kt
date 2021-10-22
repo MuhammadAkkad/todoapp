23b04e9f65f18a251411164f91b4d7c4bd7bf98c
Muhammad AKKAD <muhammed.akkad@ext.kocsistem.com.tr>
2021-10-22 12:49:26 +0300

    first design implementations

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.todoapp
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import com.example.todoapp.R
 
-class MainActivity : WearableActivity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Enables Always-on
-        setAmbientEnabled()
     }
 }
